3b8df210794aae25872dbe79f76af26ad7e73fd7
D-litchR9 <dacanaveralo@udistrital.edu.co>
2026-05-11 20:52:50 -0500

    Ganancia total agregada en el CDT

--- src/main/java/cdt/bean/CDT.java
+++ src/main/java/cdt/bean/CDT.java
@@ -21,7 +21,7 @@ public class CDT implements Serializable{
 	private double ganancia;
 	private double valorFuturo;
 	private double impuesto;
-	
+	private double gananciaNeta;
 	private static final double ANIO = 360;
 	
 	public CDT() {
@@ -35,59 +35,42 @@ public class CDT implements Serializable{
 		this.plazo = plazo;
 		
 	}
+	public double getGananciaNeta() {return gananciaNeta;}
+	public void setGananciaNeta(double gananciaNeta) {this.gananciaNeta=gananciaNeta;}
+	
 
-	public double getInversion() {
-		return inversion;
-	}
+	public double getInversion() {return inversion;}
 
-	public void setInversion(double inversion) {
-		this.inversion = inversion;
-	}
+	public void setInversion(double inversion) {this.inversion = inversion;}
 
-	public double getInteres() {
-		return interes;
-	}
+	public double getInteres() {return interes;}
 
-	public void setInteres(double interes) {
-		this.interes = interes;
-	}
+	public void setInteres(double interes) {this.interes = interes;}
 
-	public double getPlazo() {
-		return plazo;
-	}
+	public double getPlazo() {return plazo;}
 
-	public void setPlazo(double plazo) {
-		this.plazo = plazo;
-	}
+	public void setPlazo(double plazo) {this.plazo = plazo;}
 
-	public double getGanancia() {
-		return ganancia;
-	}
+	public double getGanancia() {return ganancia;}
 
-	public void setGanancia(double ganancia) {
-		this.ganancia = ganancia;
-	}
+	public void setGanancia(double ganancia) {this.ganancia = ganancia;}
 
-	public double getValorFuturo() {
-		return valorFuturo;
-	}
+	public double getValorFuturo() {return valorFuturo;}
 
-	public void setValorFuturo(double valorFuturo) {
-		this.valorFuturo = valorFuturo;
-	}
+	public void setValorFuturo(double valorFuturo) {this.valorFuturo = valorFuturo;}
 	
 	public void calcular() {
 	   
 	    double interesDecimal = interes / 100.0; 
-	    
-	    
+	       
 	    ganancia = inversion * (interesDecimal * ((double) plazo / ANIO));
-	    
-	    System.out.println("ganancia: " + ganancia);
 	    valorFuturo = inversion + ganancia;
-	    System.out.println("valorFuturo: " + valorFuturo);
-	    
 	    impuesto = ganancia * 0.04;
+	    gananciaNeta = ganancia - impuesto;
+	 
+	    System.out.println("ganancia: " + ganancia);
+	    System.out.println("valorFuturo: " + valorFuturo);
+	    System.out.println("Ganancia neta: "+ gananciaNeta);	    
 	}
 
 	
